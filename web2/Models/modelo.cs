855259c5ef6f0ebe37e9c3f9454f0fe046cccd82
Walter <silvawalteralipio@gmail.com>
2023-01-16 11:11:30 -0300

    Add method Update do Cadastro

--- web2/Models/modelo.cs
+++ web2/Models/modelo.cs
@@ -68,6 +68,19 @@ namespace web2.Models
     public string UF { get; set; } = "";
     [Required(ErrorMessage = "CEP é obrigatório")]
     public string CEP { get; set; } = "";
+
+    internal void Update(Cadastro novoCadastro)
+    {
+      this.Bairro = novoCadastro.Bairro;
+      this.CEP = novoCadastro.CEP;
+      this.Email = novoCadastro.Email;
+      this.Endereco = novoCadastro.Endereco;
+      this.Municipio = novoCadastro.Municipio;
+      this.Nome = novoCadastro.Nome;
+      this.Telefone = novoCadastro.Telefone;
+      this.UF = novoCadastro.UF;
+
+    }
   }
 
   [DataContract]
